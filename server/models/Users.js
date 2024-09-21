404d3999313127d79b27d23f31595eb65fad022d
Shreyas2004wagh <shreyaswagh2004@gmail.com>
2024-09-21 11:34:00 +0530

    errors solved

--- server/models/Users.js
+++ server/models/Users.js
@@ -4,16 +4,19 @@ const UserDetails = new mongoose.Schema(
   {
     firstName: String,
     lastName: String,
-    email: { type: String, unique: true },
+    email: { type: String, unique: true, required: true },
     password: String,
-    username: { type: String, unique: true },
+    username: { type: String, unique: true, required: true },
     likedMessages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
     bookmarkedMessages: [
       { type: mongoose.Schema.Types.ObjectId, ref: "Message" },
-    ], 
+    ],
   },
   { versionKey: false }
 );
+
+UserDetails.index({ email: 1 }, { unique: true });
+UserDetails.index({ username: 1 }, { unique: true });
 
 const Details = mongoose.model("user", UserDetails);
 
