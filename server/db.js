3f38253001e173c27d5f9cb11d6eeaf29d7bf8f7
Shreyas2004wagh <shreyaswagh2004@gmail.com>
2024-10-02 18:28:02 +0530

    resolved db.js

--- server/db.js
+++ server/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cron = require("node-cron");
 const { Details } = require("./models/Users.js");
 
 const loadEnv = async () => {
@@ -18,7 +17,6 @@ let connected = async () => {
     console.log("Connecting to the database...");
     await mongoose.connect(process.env.database_URI);
     console.log("Database connected successfully");
-    scheduleCronJobs();
   } catch (error) {
     console.error("Error connecting to the database:", error);
   }
@@ -26,34 +24,6 @@ let connected = async () => {
 
 const isConnected = () => {
   return mongoose.connection.readyState === 1;
-};
-
-const scheduleCronJobs = () => {
-  // Run every minute
-  cron.schedule("* * * * *", () => {
-    try {
-      console.log("Running a task every minute");
-    } catch (error) {
-      console.error("Error in every-minute cron job:", error);
-    }
-  });
-
-  cron.schedule("0 0 * * *", async () => {
-    try {
-      console.log("Running daily tasks at midnight");
-      console.log("Running a daily task at midnight");
-
-      // Task 2: Deleting old OTP records
-      console.log("Running cron job to delete old OTP records");
-      const result = await Details.updateMany(
-        { otpExpiration: { $lt: new Date() } }, // Find expired OTPs
-        { $unset: { otp: "", otpExpiration: "" } } // Remove fields
-      );
-      console.log(`Cleared OTPs from ${result.modifiedCount} user records`);
-    } catch (error) {
-      console.error("Error in daily midnight cron job:", error);
-    }
-  });
 };
 
 module.exports = {
